162c6646ded70ba75c4928e91193283ba3fdd418
Chapiuskys <gabrielmp2709@gmail.com>
2025-09-08 16:42:48 -0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
 "use client"
 import { useState } from "react"
 import { Button } from "./_components/ui/button"
-
 const Home = () => {
   const [] = useState()
-  return <Button>Hello world!</Button>
+  return              <Button>Hello world!</Button>
 }
 
 export default Home
