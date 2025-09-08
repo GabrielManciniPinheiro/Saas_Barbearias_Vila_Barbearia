29fc205c689df0e7a584681d30c1b4550d2df100
Chapiuskys <gabrielmp2709@gmail.com>
2025-09-08 16:59:42 -0300

    Funfando teste

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 import { Button } from "./_components/ui/button"
 const Home = () => {
   const [] = useState()
-  return              <Button>Hello world!</Button>
+  return <Button>Hello world!</Button>
 }
 
 export default Home
