3540dab10aed07c2725710c531eb6b97fe9919bb
Chapiuskys <gabrielmp2709@gmail.com>
2025-09-09 13:14:17 -0300

    feat: add header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,40 @@
 "use client"
 import { useState } from "react"
+import Header from "./_components/header"
+import { Input } from "./_components/ui/input"
 import { Button } from "./_components/ui/button"
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
+
 const Home = () => {
   const [] = useState()
-  return <Button>Hello world!</Button>
+  // import Header from "./_components/header"
+  return (
+    <div>
+      {/* <Header /> */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Gabriel!</h2>
+        <p>Terça-Feira, 05 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button className="bg-indigo-800 text-white hover:bg-indigo-700">
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            alt="Agende seu atendimento no melhor: Vila Barbearia!"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
